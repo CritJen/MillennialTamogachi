770b3612c0515144e14c53c5c284406e3815d4bc
github username <github email address>
2019-05-04 22:35:10 -0700

    Basic formatting & Millennial Pink

--- MT-frontend/millennial-tamogachi-client/src/containers/FormContainer.js
+++ MT-frontend/millennial-tamogachi-client/src/containers/FormContainer.js
@@ -1,12 +1,12 @@
 import React from "react";
 import man from "../assets/avatar/hipster-man.svg";
 import woman from "../assets/avatar/hipster-woman.svg";
-import { Form, Button, Modal } from "semantic-ui-react";
+import { Form, Button, Modal, Input, Checkbox } from "semantic-ui-react";
+import styles from "../styles/FormContainer.module.css";
 // Setting Constants
 const MILLENNIALS_URL = "http://localhost:4000/api/v1/millennials";
 
 class FormContainer extends React.Component {
-
   constructor() {
     super();
     this.state = {
@@ -51,7 +51,7 @@ class FormContainer extends React.Component {
           name: "",
           avatar: ""
         });
-        console.log(data)
+        console.log(data);
         this.props.handleNewMillennial(data);
       });
   };
@@ -64,44 +64,46 @@ class FormContainer extends React.Component {
         closeOnDimmerClick
         closeIcon
         onClose={this.props.closeModal}
+        className={styles.form}
       >
-        <Modal.Header>Form</Modal.Header>
-        <Modal.Content>
-          <input
-            type="text"
-            className="ui input"
-            name="name"
-            placeholder="Name"
-            value={this.state.name}
-            onChange={this.handleChange}
-            required
-          />
-          {/* Gender */}
-          <Form.Group inline>
-            <label>Gender</label>
-            <Form.Radio
-              label="Male"
-              value="male"
-              checked={this.state.gender === "Male"}
-              onChange={this.handleRadioChange}
+        <Modal.Header className={styles.brown}>Form</Modal.Header>
+        <Modal.Content className={styles.blue}>
+          <Form>
+            <Form.Input
+              label="Name"
+              type="text"
+              name="name"
+              placeholder="Name"
+              value={this.state.name}
+              onChange={this.handleChange}
+              required
             />
-            <Form.Radio
-              label="Female"
-              value="female"
-              checked={this.state.gender === "Female"}
-              onChange={this.handleRadioChange}
-            />
-          </Form.Group>
-          {/* Submit */}
-          <Button type="submit" onClick={this.postMillennial}>
-            Create Your Millennial
-          </Button>
+            {/* Gender */}
+            <Form.Group inline>
+              <label>Present as:</label>
+              <Form.Radio
+                label="Male"
+                value="male"
+                checked={this.state.gender === "Male"}
+                onChange={this.handleRadioChange}
+              />
+              <Form.Radio
+                label="Female"
+                value="female"
+                checked={this.state.gender === "Female"}
+                onChange={this.handleRadioChange}
+              />
+            </Form.Group>
+            {/* Submit */}
+            <Button color="teal" type="submit" onClick={this.postMillennial}>
+              Create Your Millennial
+            </Button>
+          </Form>
           {this.state.gender === "Male" ? (
             <img src={man} alt="man" />
           ) : (
             <img src={woman} alt="woman" />
           )}
-
         </Modal.Content>
       </Modal>
     );
